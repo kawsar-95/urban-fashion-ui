625a0a56461e777e3353d4daf6161afba284246c
kawsar-95 <virtualkawsar@gmail.com>
2021-04-11 18:27:43 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
 import Home from './components/Home/Home';
 
-
-
 function App() {
   return (
     <div>
@@ -10,5 +8,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
